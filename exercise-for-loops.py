042d60ff4275428bc75c868ff72cb6bb08c2b6c3
scootscootscootscoot <scott.cahill@protonmail.com>
2025-07-16 15:53:48 -0500

    loops and arrays-7-16

---
+++ exercise-for-loops.py
@@ -0,0 +1,21 @@
+#play with for loops
+
+
+
+
+#create a list
+random_list = []
+print("Your list:", random_list)
+
+random_list.append("alpha")
+random_list.append("beta")
+random_list.append("delta")
+random_list.append("epsilon")
+random_list.append("gamma")
+random_list.append("zeta")
+
+
+#use a loop to iterate through the list 
+print("Heres a random list of things")
+for task in random_list:
+    print("- ", task)
